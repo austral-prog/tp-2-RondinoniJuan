b2c8f52143712ba9021b5dbc38d1bbe6695c4a93
RondinoniJuan <162723034+RondinoniJuan@users.noreply.github.com>
2024-03-22 12:58:44 -0300

    Tp2 "Change" entregado

--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    
+    print("Vuelto")
+    
+    print("Pesos:")
+    print(int((money - expense) - 0.25))
+    print("Centavos:")
+    print(int(((money - expense) - (int((money - expense) - 0.25))) * 100))
